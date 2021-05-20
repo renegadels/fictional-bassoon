6a9c37e5b4b123eed3ab1931bfed45ecb98d6f0e
renegadels <58387535+renegadels@users.noreply.github.com>
2021-05-20 20:29:59 +0530

    Update and rename mapScript.js to etaBox.js

---
+++ etaBox.js
@@ -0,0 +1,22 @@
+const chatbox = jQuery.noConflict();
+
+chatbox(() => {
+  chatbox(".chatbox-open").click(() =>
+    chatbox(".chatbox-popup, .chatbox-close").fadeIn()
+  );
+
+  chatbox(".chatbox-close").click(() =>
+    chatbox(".chatbox-popup, .chatbox-close").fadeOut()
+  );
+
+
+  chatbox(".chatbox-minimize").click(() => {
+    chatbox(".chatbox-panel").fadeOut();
+    chatbox(".chatbox-popup, .chatbox-open, .chatbox-close").fadeIn();
+  });
+
+  chatbox(".chatbox-panel-close").click(() => {
+    chatbox(".chatbox-panel").fadeOut();
+    chatbox(".chatbox-open").fadeIn();
+  });
+});
